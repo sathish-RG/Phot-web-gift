66d96fdbb7dab57b65cec5cf3876458f3ed47945
sathish <sathishravichandran9345@gmail.com>
2026-04-17 23:41:37 +0530

    count

--- birthday-gallery/src/data/photos.js
+++ birthday-gallery/src/data/photos.js
@@ -1,5 +1,4 @@
 export const photos = [
-  { id: 1776448516354, name: "Best Friends", type: "wish", wish: "Many more happy returns of the day!\nMay all your dreams come true.\n\n                                  - Nikhila & Sivasangari", tag: "Best Friend", mystery: false, image: "/uploads/img_1776448516354.jpg", color: "#e879f9" },
   { id: 1776445459244, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445459243.jpg", color: "#e879f9" },
   { id: 1776445458370, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445458370.jpg", color: "#e879f9" },
   { id: 1776445457490, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445457489.jpg", color: "#e879f9" },
@@ -18,7 +17,7 @@ export const photos = [
   { id: 1776445235268, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445235267.jpg", color: "#e879f9" },
   { id: 1776445200349, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445200348.jpg", color: "#e879f9" },
   { id: 1776445095323, name: "", type: "normal", wish: "", tag: "Best Friend", mystery: false, image: "/uploads/img_1776445095322.jpg", color: "#e879f9" },
-  
+  { id: 1776448516354, name: "Best Friends", type: "wish", wish: "Many more happy returns of the day!\nMay all your dreams come true.\n\n                                  - Nikhila & Sivasangari", tag: "Best Friend", mystery: false, image: "/uploads/img_1776448516354.jpg", color: "#e879f9" }
 ];
 
 export const tags = ["All", "Best Friend", "Family", "Colleague"];
